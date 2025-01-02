a0ec12181d68d8629ee1cc3e1226ad01d4b8c9be
Kristen Swaim <106117219+kdswaim@users.noreply.github.com>
2025-01-01 20:51:00 -0500

    final draft of landing page version

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+PUBLIC_CLOUDINARY_CLOUD_NAME="dqfotf1jf"
+
+PUBLIC_CLOUDINARY_API_KEY="117527372545423"
+CLOUDINARY_API_SECRET="LuH9dHjb_REc6n5A__hDrPCEk-U"
